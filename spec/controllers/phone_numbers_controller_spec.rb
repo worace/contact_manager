12b1f911e612ba4c0e2293f6f8ae65a9ca7c41e9
worace <horace.d.williams@gmail.com>
2014-11-11 09:22:57 -0700

    Update phone number form to automatically take person ID from query param

--- spec/controllers/phone_numbers_controller_spec.rb
+++ spec/controllers/phone_numbers_controller_spec.rb
@@ -51,6 +51,9 @@ RSpec.describe PhoneNumbersController, :type => :controller do
 
   describe "POST create" do
     describe "with valid params" do
+      let(:alice) { Person.create(:first_name => "Alice", :last_name => "Smith") }
+      let(:valid_attributes) { {:number => '555-8888', :person_id => alice.id} }
+
       it "creates a new PhoneNumber" do
         expect {
           post :create, {:phone_number => valid_attributes}, valid_session
@@ -63,9 +66,9 @@ RSpec.describe PhoneNumbersController, :type => :controller do
         expect(assigns(:phone_number)).to be_persisted
       end
 
-      it "redirects to the created phone_number" do
+      it "redirects to the phone number's person" do
         post :create, {:phone_number => valid_attributes}, valid_session
-        expect(response).to redirect_to(PhoneNumber.last)
+        expect(response).to redirect_to(alice)
       end
     end
 
