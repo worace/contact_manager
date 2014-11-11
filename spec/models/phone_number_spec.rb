b55c27fe438f59e131e39fcd9b51fc8f2e8f4dc6
worace <horace.d.williams@gmail.com>
2014-11-10 17:35:50 -0700

    Require a phone number for a person

--- spec/models/phone_number_spec.rb
+++ spec/models/phone_number_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe PhoneNumber, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:phone_number) { PhoneNumber.new(number: "1112223333") }
+
+  it "is valid" do
+    expect(phone_number).to be_valid
+  end
+
+  it "is invalid without a number" do
+    phone_number.number = nil
+    expect(phone_number).not_to be_valid
+  end
 end
