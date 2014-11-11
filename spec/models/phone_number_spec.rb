269ae53216ed6a11e778af58ce7b0c55c108cb15
worace <horace.d.williams@gmail.com>
2014-11-10 17:39:27 -0700

    test phone number validations and updating in controller

--- spec/models/phone_number_spec.rb
+++ spec/models/phone_number_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe PhoneNumber, :type => :model do
-  let(:phone_number) { PhoneNumber.new(number: "1112223333") }
+  let(:phone_number) do
+    PhoneNumber.new(number: "1112223333", person_id: 1)
+  end
 
   it "is valid" do
     expect(phone_number).to be_valid
@@ -9,6 +11,11 @@ RSpec.describe PhoneNumber, :type => :model do
 
   it "is invalid without a number" do
     phone_number.number = nil
+    expect(phone_number).not_to be_valid
+  end
+
+  it "must have a reference to a person" do
+    phone_number.person_id = nil
     expect(phone_number).not_to be_valid
   end
 end
