d7e24750642c4afdb07b78f5c114674ad241a31d
worace <horace.d.williams@gmail.com>
2014-11-12 09:24:32 -0700

    initial companies scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :companies
+
   resources :email_addresses
 
   resources :phone_numbers
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141112150523) do
+ActiveRecord::Schema.define(version: 20141112162133) do
+
+  create_table "companies", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "email_addresses", force: true do |t|
     t.integer  "person_id"
