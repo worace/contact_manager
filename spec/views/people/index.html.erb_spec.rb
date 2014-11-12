443764f7866b301169357c797617da26d2c32489
worace <horace.d.williams@gmail.com>
2014-11-12 12:20:56 -0700

    some view refactorig

--- spec/views/people/index.html.erb_spec.rb
+++ spec/views/people/index.html.erb_spec.rb
@@ -16,7 +16,6 @@ RSpec.describe "people/index", :type => :view do
 
   it "renders a list of people" do
     render
-    assert_select "tr>td", :text => "First Name".to_s, :count => 2
-    assert_select "tr>td", :text => "Last Name".to_s, :count => 2
+    assert_select "tr>td", :text => "First Name Last Name".to_s, :count => 2
   end
 end
