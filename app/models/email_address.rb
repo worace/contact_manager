9e3048dbe0079051e66f9e0e3ebf18a9e1f3616d
worace <horace.d.williams@gmail.com>
2014-11-12 08:24:38 -0700

    flushing out validations for email addresses

--- app/models/email_address.rb
+++ app/models/email_address.rb
@@ -1,3 +1,4 @@
 class EmailAddress < ActiveRecord::Base
   belongs_to :person
+  validates :address, :person_id, presence: true
 end
--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe EmailAddress, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:email) { EmailAddress.new(:address => "hi@pizza.com", :person_id => 1) }
+
+  it "is valid with an address" do
+    email.address = "hi@example.com"
+    assert email.valid?
+  end
+
+  it "is invalid without address" do
+    email.address = nil
+    refute email.valid?
+  end
+
+  it "is invalid without person id" do
+    email.person_id = nil
+    refute email.valid?
+  end
 end
