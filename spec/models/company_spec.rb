5a3322a6a513801496d942efee65ee501215f3db
worace <horace.d.williams@gmail.com>
2014-11-12 10:53:30 -0700

    add more robust relationship tests for people and companies

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -15,4 +15,9 @@ RSpec.describe Company, :type => :model do
   it "has an array of phone numbers" do
     expect(company.phone_numbers).to eq([])
   end
+
+  it "responds with its phone numbers after they're created" do
+    phone_number = company.phone_numbers.build(number: "333-4444")
+    expect(phone_number.number).to eq('333-4444')
+  end
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -24,4 +24,14 @@ RSpec.describe Person, :type => :model do
   it "has an array of email addresses" do
     expect(person.email_addresses).to eq([])
   end
+
+  it 'responds with its created phone numbers' do
+    person.phone_numbers.build(number: '555-8888')
+    expect(person.phone_numbers.map(&:number)).to eq(['555-8888'])
+  end
+
+  it 'responds with its created email addresses' do
+    person.email_addresses.build(address: 'me@example.com')
+    expect(person.email_addresses.map(&:address)).to eq(['me@example.com'])
+  end
 end
